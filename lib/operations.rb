a7395dff193c9975dc7aa167adc024a78efc573a
Talisa Erazo <talisaerazo@gmail.com>
2018-10-13 13:17:55 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	else speed = 40..60
+		return false
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+ 	speed < 40 ? true :
+	speed > 60 ? true : false
 end
-	
-
-
